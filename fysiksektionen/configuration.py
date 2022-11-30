639a45a065bfd097c9a00714c08fe4c5c70531b3
Elias Lundell <elilun03@gmail.com>
2022-11-30 18:32:15 +0100

    New structure

--- fysiksektionen/configuration.py
+++ fysiksektionen/configuration.py
@@ -2,11 +2,11 @@ TEAM_ID = "71bh96izu7f1iee7ayhuan9itr"
 CHANNELS = {
         "sektionsnytt": "pda7c6r7zbbtmc17x4ehczc8gc",
         "evenemang": "wkgnsmmbqpgpj8ptdiyx8os16c",
-        "rekrytering": "4ixiiwsqbfroiretgpmmcnrwor",
+        "general": "wy6t5d3tapf5pfbgxq9onp1upe",
         "random": "6onymh6hsjyk3ef5q879wzt9fh"
         }
 
 CATEGORIES = {
-        "Fysiksektionen": ["sektionsnytt", "evenemang", "rekrytering"],
-        "Channels": ["random"]
+        "Fysiksektionen": ["sektionsnytt", "evenemang"],
+        "Channels": ["random", "general"]
         }
