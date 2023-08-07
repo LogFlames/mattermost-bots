8960b55fddd7062cdc8ea3e4f486c04590b0ae6b
Elias Lundell <elilun03@gmail.com>
2023-08-07 10:17:57 +0200

    Add parent dir to python path

--- mottagningen/eliasmamo_import.py
+++ mottagningen/eliasmamo_import.py
@@ -2,7 +2,7 @@ from mattermostdriver import Driver
 import os
 import sys
 
-parent_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
 sys.path.append(parent_dir)
 
 from helper_functions.eliasmamo import add_to_default_channels, delete_new_posts_in_clean_channels
