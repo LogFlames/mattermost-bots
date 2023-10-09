0ae7a566438f84b3af666bb562bee02ac05ea46b
Elias Lundell <elilun03@gmail.com>
2023-10-09 18:43:51 +0200

    valvakan

---
+++ valvakan/configuration.py
@@ -0,0 +1,2 @@
+CHANNEL_ID = "y58scewz9bnri8t8i5p4ec5coo"
+GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1Z5cXCBeeVf2Qbu6qARLpX8lm6ZCOTVQps8LRuWZnTDw/edit#gid=0"
---
+++ valvakan/main.py
@@ -0,0 +1,91 @@
+from eliasmamo_import import *
+from secret import TOKEN
+from configuration import *
+import time
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1Z5cXCBeeVf2Qbu6qARLpX8lm6ZCOTVQps8LRuWZnTDw'
+SAMPLE_RANGE_NAME = 'Blad1!A2:E'
+
+def read_sheet():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        return values
+    except HttpError as err:
+        print(err)
+
+def main():
+    driver = Driver(
+            {
+                'url': 'mattermost.fysiksektionen.se',
+                'basepath': '/api/v4',
+                'verify': True,
+                'scheme': 'https',
+                'port': 443,
+                'auth': None,
+                'token': TOKEN,
+                'keepalive': True,
+                'keepalive_delay': 5,
+                }
+            )
+
+    driver.login()
+
+    prev_vals = read_sheet()
+
+    while True:
+        time.sleep(10)
+        v = read_sheet()
+
+        if v is None:
+            print(f"Falied to read sheet values")
+            continue
+
+        for row in v:
+            if row not in prev_vals:
+                print(f"@channel {row[1]} {row[2]} {row[3]} har sökt {row[4]}")
+
+    # User addad to team -> Add to channel {'event': 'user_added', 'data': {'team_id': 'g16tqepa3ffntkfnnwqyapkzkr', 'user_id': 'zu7i4ow3obfa3egwpau59r6s4a'}, 'broadcast': {'omit_users': None, 'user_id': '', 'channel_id': '8e9yhhagtjbnpdyr6eiox8i3oa', 'team_id': '', 'connection_id': ''}, 'seq': 8}
+
+if __name__ == "__main__":
+    main()
