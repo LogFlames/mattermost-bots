e2bd90e280c651908a5227f50a350b0598ba2b99
Elias Lundell <elilun03@gmail.com>
2023-11-27 18:00:40 +0100

    Add english reaction functionality

--- fysiksektionen/configuration.py
+++ fysiksektionen/configuration.py
@@ -3,10 +3,18 @@ CHANNELS = {
         "sektionsnytt": "pda7c6r7zbbtmc17x4ehczc8gc",
         "evenemang": "wkgnsmmbqpgpj8ptdiyx8os16c",
         "general": "wy6t5d3tapf5pfbgxq9onp1upe",
-        "random": "6onymh6hsjyk3ef5q879wzt9fh"
+        "random": "6onymh6hsjyk3ef5q879wzt9fh",
+        "info_and_urls": "dz1jak398fyhir1uxpya7grify",
+        "events": "it7zah8m5jrw9dm8bkjm14eyaw"
         }
 
+DEFAULT_CHANNELS = ["sektionsnytt", "evenemang", "general", "random"]
+ENGLISH_CHANNELS = ["events"]
+
 CATEGORIES = {
-        "Fysiksektionen": ["sektionsnytt", "evenemang"],
+        "Fysiksektionen": ["sektionsnytt", "evenemang", "info_and_urls"],
+        "Physics Chapter": ["events"],
         "Channels": ["random", "general"]
         }
+
+MESSAGE_WITH_ENGLISH_REACTIONS_POST_ID = "zdr5f5n8jibzpq68xr6guoch3h"
--- fysiksektionen/main.py
+++ fysiksektionen/main.py
@@ -1,11 +1,55 @@
 from eliasmamo_import import *
 from secret import TOKEN
 from configuration import *
+import json
+from tqdm import tqdm
+
+def fix_users(driver):
+    print("Fixing new users default channels...")
+    users = get_team_members(driver, TEAM_ID)
+    reactions = driver.reactions.get_reactions_of_post(MESSAGE_WITH_ENGLISH_REACTIONS_POST_ID)
+
+    print("Getting reactions...")
+    user_in_english = {user: False for user in users}
+    for reaction in reactions:
+        if reaction["post_id"] == MESSAGE_WITH_ENGLISH_REACTIONS_POST_ID and reaction["emoji_name"] in ("english", "en_english"):
+            user_in_english[reaction["user_id"]] = True
+
+    print("Getting reactions... Done")
+    for user_id in tqdm(users):
+        channels_to_add_to = {channel: CHANNELS[channel] for channel in DEFAULT_CHANNELS}
+
+        if user_in_english[user_id]:
+            channels_to_add_to |= {channel: CHANNELS[channel] for channel in ENGLISH_CHANNELS}
+
+        add_to_default_channels(driver, {"user_id": user_id, "team_id": TEAM_ID}, TEAM_ID, channels_to_add_to)
+
+        manage_channel_categories(driver, user_id, TEAM_ID, CHANNELS, CATEGORIES)
+        enable_all_notifications(driver, user_id)
+    print("Fixing new users default channels ... Done")
+
 
 def new_user(driver, data):
-    add_to_default_channels(driver, data, TEAM_ID, CHANNELS)
+    channels_to_add_to = {channel: CHANNELS[channel] for channel in DEFAULT_CHANNELS}
+
+    add_to_default_channels(driver, data, TEAM_ID, channels_to_add_to)
+
     manage_channel_categories(driver, data["user_id"], TEAM_ID, CHANNELS, CATEGORIES)
     enable_all_notifications(driver, data["user_id"])
+
+def add_to_english(driver, user_id):
+    channels_to_add_to = {channel: CHANNELS[channel] for channel in ENGLISH_CHANNELS}
+
+    add_to_default_channels(driver, {"user_id": user_id, "team_id": TEAM_ID}, TEAM_ID, channels_to_add_to)
+
+    manage_channel_categories(driver, user_id, TEAM_ID, CHANNELS, CATEGORIES)
+
+def reacted(driver, data):
+    reaction = json.loads(data["reaction"])
+
+    if reaction["post_id"] == MESSAGE_WITH_ENGLISH_REACTIONS_POST_ID:
+        if reaction["emoji_name"] in ("english", "en_english"):
+            add_to_english(driver, reaction["user_id"])
 
 def main():
     driver = Driver(
@@ -27,14 +71,12 @@ def main():
 
     print("Listening for new users")
     ws.subscribe("user_added", lambda data: new_user(driver, data))
+    ws.subscribe("reaction_added", lambda data: reacted(driver, data))
 
     delete_new_posts_in_clean_channels(driver, CHANNELS)
 
     if False:
-        print("Adding new users to default channels...")
-        for user in get_team_members(driver, TEAM_ID):
-            new_user(driver, {"user_id": user, "team_id": TEAM_ID})
-        print("Adding new users to default channels ... Done")
+        fix_users(driver)
 
     ws.join()
 
