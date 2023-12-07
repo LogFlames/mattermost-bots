405951683c065e3b51fbd3058ba412dd332496f8
Elias Lundell <elilun03@gmail.com>
2023-12-07 23:44:06 +0100

    rename swedish and english channels

--- fysiksektionen/configuration.py
+++ fysiksektionen/configuration.py
@@ -4,8 +4,8 @@ CHANNELS = {
         "evenemang": "wkgnsmmbqpgpj8ptdiyx8os16c",
         "general": "wy6t5d3tapf5pfbgxq9onp1upe",
         "random": "6onymh6hsjyk3ef5q879wzt9fh",
-        "info_and_urls": "dz1jak398fyhir1uxpya7grify",
-        "info_och_länkar": "1kfcce1i7py4ip1933fkb95jrh",
+        "info": "dz1jak398fyhir1uxpya7grify",
+        "info_eng": "1kfcce1i7py4ip1933fkb95jrh",
         "events": "it7zah8m5jrw9dm8bkjm14eyaw"
         }
 
@@ -13,8 +13,8 @@ DEFAULT_CHANNELS = ["sektionsnytt", "evenemang", "general", "random"]
 ENGLISH_CHANNELS = ["events"]
 
 CATEGORIES = {
-        "Fysiksektionen": ["sektionsnytt", "evenemang", "info_och_länkar"],
-        "Physics Chapter": ["events", "info_and_urls"],
+        "Fysiksektionen": ["sektionsnytt", "evenemang", "info"],
+        "Physics Chapter": ["events", "info_eng"],
         "Channels": ["random", "general"]
         }
 
