38e76a1f44afc3181b282c07bfbb9667a1234a0a
Elias Lundell <elilun03@gmail.com>
2023-12-12 23:12:00 +0100

    english translations of about this, links and resources

--- info_channel/messages_eng.py
+++ info_channel/messages_eng.py
@@ -14,17 +14,96 @@ Chapter Officials are those who the chapter has elected and entrusted to perform
 Other questions should be sent to styret@f.kth.se."""],
         ["""## Links to resources
 The Physics Chapter has a website and Google Drive. In addition to this the student union THS and the university KTH has several resources which may be of interest to you. Below are some resources which we have curated into easily accessible lists."""],
-        ["##### **:fraktur-f: Chapter Website** — f.kth.se", "<Insert beskrivning av hemsidan och länkar till rimliga delar>"],
-        ["##### **:google_drive: Google Workspace** — Drive, mail & kalender", "<Insert text>"],
-        ["##### :fysiksektionen: The Physics Chapter — Styrdokument & sektionsinformation", "<Insert text>"],
-        ["##### :ths: THS — The Student Union", "<Insert text>"],
+        ["##### **:fraktur-f: Chapter Website** — f.kth.se", 
+         """The chapter hosts a website where there is a lot of useful information and contact information. Among these are a digital version of the songbook, links to meeting documents and governing documents. On the main page, there is also a display of some of the chapter's events and a large banner with important information.
+
+* [f.kth.se](https://f.kth.se/)
+* [Songbook](https://f.kth.se/sangbok/#)
+* [Governing documents](https://f.kth.se/sektionen/dokument/styrdokument)
+* [Information about and links to all committees](https://f.kth.se/namnder/)""",
+         """On the website there is a contact form for the chapter's student safety representative, SSO. The chapter's student safety representative has a duty of confidentiality according to the law and is there to talk to if you want to talk about something (anything!) or report something that has happened in connection with the chapter or KTH. Report what you find out, even if you don't want any action to be taken! It helps the chapter's preventive work.
+* [Contact form for the chapter's SSO (Student Social Welfare Representative)](https://f.kth.se/skyddsombud/)
+The chapter's student safety representative, @erikasan, can also be contacted on Mattermost."""],
+        ["##### **:google_drive: Google Workspace** — Drive, mail & calender",
+         """The chapter provides a Google Workspace with accounts at @fysiksektionen.se and @f.kth.se. All chapter members receive an account and thus have access to
+- the chapter's Google Drive — where many files and documents related to the chapter are available
+- the chapter's Gmail — where chapter members can receive and send emails in chapter contexts
+- as well as the chapter's calendars — where chapter members can get an overview of upcoming events and room bookings.""",
+         """##### Access to Google Workspace :closed_lock_with_key:
+* For information on how to log in to your account, see [Log in to your fysiksektionen.se account](https://docs.google.com/document/d/1QcuIxhI4yj-lGqAv2d4rmcgoCSgNSi1vhPKf0A7AKUY/edit?usp=sharing). There, there is also information on how to access the chapter's Google Drive, Gmail and Google Calendar.""",
+         """##### The Chapter's Google Drive :google_drive:
+In the Chapter's Google Drive you can find governing documents, guides, many drives for the chapter's various groups and committees, and much more. All chapter members have access to the drives 'Fysiksektionen — Alla | Everyone' and 'Fysiksektionen — Funktionärer'.
+
+**In 'Fysiksektionen — Alla'** you can find things that are interesting and should be available to all members, such as certain guides, the chapter's Swedish-English dictionary, governing documents, and financial reports.
+
+**In 'Fysiksektionen — Funktionärer'** you can find things that are of interest to everyone involved in the Physics Chapter, such as many guides and templates, and much more. Sensitive information is not stored here.
+
+* General link: [drive.fysiksektionen.se](https://drive.fysiksektionen.se)
+* The Drive 'Fysiksektionen — Alla': [link](https://drive.google.com/drive/folders/0AAem0L2dOsvqUk9PVA)
+* The Drive 'Fysiksektionen — Funktionärer': [link](https://drive.google.com/drive/folders/0AAPbyBpwLHDEUk9PVA)""",
+         """##### The Chapter's Google Mail, Gmail :gmail:
+
+The Chapter's Gmail (referred to as the chapter mail) gives you access to send and receive emails on your @fysiksektionen.se account. The chapter often sends information to this account, so it may be relevant to be logged into the account on your mobile device for easy access to your chapter emails.
+
+* General link: [mail.fysiksektionen.se](https://maik.fysiksektionen.se)
+* Link to the app [Gmail on iOS (Apple)](https://apps.apple.com/se/app/gmail-e-post-fr%C3%A5n-google/id422689480)
+* Link to the app [Gmail on Android](https://play.google.com/store/apps/details?id=com.google.android.gm&hl=en)
+* If you have a position at the chapter: [Guide to e-mail for officials](https://docs.google.com/document/d/1LAdrim-KZ8D6jFziRFPtBJVBSojiOG2eR6BskuXJ2r0/edit?usp=drive_link)""",
+         """##### The Chapter's Google Calendar :google_calendar:
+The Chapter uses Google Calendar so that committees can book events among themselves. Checking the calendar is an excellent way to keep track of upcoming events and get an idea of which events you might want to attend later.
+
+* Link to a [direct view of the chapter calendar](https://calendar.google.com/calendar/u/0/embed?wkst=2&bgcolor=%23FF642B&ctz=Europe/Stockholm&mode=WEEK&hl=en&title=Fysiksektionen&showPrint=0&showTz=0&src=ZnlzaWtzZWt0aW9uZW4uc2Vfb3M5djlnY21vNXMwcnFmOTZlbXJtODVnOW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZnlzaWtzZWt0aW9uZW4uc2VfMDE4N3ZibWRjaXZsOG10aW8xNDJlMjNjYXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23EE642B&color=%23FF642B%22+style%3D%22border-width:0%22+width%3D%22800%22+height%3D%22600%22+frameborder%3D%220%22+scrolling%3D%22no%22)
+* Link to the document [Guide to the chapter's shared calendars](https://docs.google.com/document/d/1-aea3X0JPqBrZdBfqa1IDSYZl2J17Qo5oedKElDCLd8/edit?usp=sharing) where instructions on how to add the calendars to your own private calendar can be found."""],
+        ["##### :fysiksektionen: The Physics Chapter — Governing documents & Info about the Chapter",
+         """#### The Physics Chapter
+The Physics Chapter is a non-profit organization that exists for all students studying Engineering Physics and Engineering Mathematics at KTH. The Physics Chapter monitors education, organizes a lot of fun events, and above all, has several active and engaging committees.
+
+For more information about the chapter, how it works, all the different committees, and how the chapter is governed, you can read [General Information about The Physics Chapter](https://docs.google.com/document/d/15VEz0WV6W6RpNgdy23X0y6qtAMCI2tUIy2K9LcrohHg/edit?usp=sharing).""",
+         """#### Meeting documents
+
+The Physics Chapter has 6 chapter meetings (SM) and several chapter board meetings (STYM) each year. Chapter meetings are the highest decision-making body of the chapter, where chapter members have the opportunity to influence the chapter and make important decisions. Meeting documents for these meetings are available in the Chapter Hall Konsulatet before the meetings and are continuously sent out in ~events here in Mattermost. If you want to find old notices and minutes, these can be found at [Meeting documents for SM and STYM](https://f.kth.se/sektionen/dokument/moteshandlingar/).""",
+         """#### Governing documents
+The Physics Chapter also has governing documents that define the chapter and govern its activities. PM (memorandum) are determined by the board, and the rest (Statutes, Budget, Policy Documents, etc.) are determined by the Chapter Meeting (SM). These can be updated by submitting a motion to any SM.
+
+Governing documents:
+* [Statutes](https://drive.google.com/file/d/1fB4_xz2JReqvYFqEtp_-WwAsqDfNOg5_/view?usp=sharing) in Swedish
+* [Bylaws](https://drive.google.com/file/d/1EbsYxrx41E850lE5S_YasqiA0iqzpy0U/view?usp=sharing) in Swedish
+* [Operational Plan](https://drive.google.com/file/d/1I77efDxtnD0gEG1EyO5GUIjcwn1j-ZE8/view?usp=share_link) in Swedish
+* [Budget](https://drive.google.com/file/d/1tHzO_Omg8x-D9LwC8hdAH5ATN-AeCXYf/view?usp=drive_link), bilingual
+* [Detail Budgets](https://drive.google.com/file/d/1pwSwha2hyDfSvXEMBOT2HhTBpLkffZdl/view?usp=drive_link), bilingual
+
+Policies:
+* [Personal Data Policy](http://f.kth.se/privacy) in English
+* [Code of Conduct](https://drive.google.com/file/d/18qvKoC5ndK691S6m1q_KZuUenFpnxDia/view?usp=sharing) in English
+* [Operational Policy - Physics Chapter Asset Management](https://docs.google.com/document/d/1xw5m96fCmEGgJYGu7RP4o4jWGmlaGQzA2cd-MXIcvp0/edit) in Swedish
+* [Alcohol Policy](https://drive.google.com/file/d/1I9ty46rnD1rebDtDMjX7wAIcESMTXYaG/view?usp=drive_link) in Swedish
+
+Memorandums (PM):
+* [PM for Economy](https://drive.google.com/file/d/1o7yChHbhqqXD_8jsovAdmrMpRDkKyE1K/view?usp=drive_link) in Swedish
+* [PM for Communication](https://drive.google.com/file/d/1hE6uFskvVY1AmR2214tEMLexBgATlH8p/view?usp=drive_link) in Swedish
+* [PM for Committees](https://drive.google.com/file/d/1FeGPQWHO4zoHxGjFIIad80dSFIC4RWMZ/view?usp=drive_link) in Swedish
+* [PM for Traditions](https://drive.google.com/file/d/1GTxa1TYBSQuZGzCnjr2NGDHjsZ0kRYqH/view?usp=drive_link) in Swedish
+* [PM for Climate and Environmental Work](https://drive.google.com/file/d/1Y9bxw2ElcK51i_-lqhrtptAwZYxoqgFb/view?usp=drive_link) in Swedish
+
+All governing documents are also compiled at [f.kth.se/sektionen/dokument/styrdokument](https://f.kth.se/sektionen/dokument/styrdokument/) and in the chapter's Google Drive."""],
+        ["##### :ths: THS — The Student Union",
+         """The Student Union (THS) is the organization that all chapters at KTH are governed by, including The Physics Chapter.
+
+On THS's website [ths.kth.se](https://ths.kth.se/) there are many helpful resources regarding your membership in the union, contact information for questions related to student life, the study environment, and KTH, as well as Nymble and Osqvik. Below are some of these resources:
+
+* THS website: [ths.kth.se](https://ths.kth.se/)
+* Information about your membership in the union, with the possibility to become a member: [ths.kth.se/login](http://ths.kth.se/login)
+* [THS official documents and governing documents](https://drive.google.com/drive/folders/1HIiGda31F5ytr9eG0IO3d0hO7xeCI-2W?usp=sharing) that partially regulate The Physics Chapter
+* Link to a calendar for [events happening at THS](https://ths.kth.se/en/events)
+* Link to [information about the premises in Nymble and Osqvik](https://ths.kth.se/en/nymble-osqvik/information-about-the-premises)
+* [Contact information for THS](https://ths.kth.se/en/contact)"""],
         ["""# About this channel
 
 **In this channel information regarding the Physics Chapter and Mattermost is hosted. In particular elected chapter officials of the Physics Chapter are listed, so that it is easy for you to locate who to contact with your questions. In addition to that some links and resources which you have access to as a member of the Physics Chapter are listed.**
 
 **This channel is an excellent starting point if you're looking for something in particular.**
 
-The swedish version of this channel is available at ~info.
+The Swedish version of this channel is available at ~info.
 """]
 ]
 
