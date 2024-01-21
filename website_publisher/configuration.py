4d2cba03cdf81019e01fd1f177a9fd0fee11c909
Elias Lundell <elilun03@gmail.com>
2024-01-21 21:29:40 +0100

    Add authentication

--- website_publisher/configuration.py
+++ website_publisher/configuration.py
@@ -25,25 +25,91 @@ EMOJI_MAP = {
 
 USERS = {
         "mattermästare": [
-            "xzt5e9rmtb8b5c78auq3h7bffr", # Elias Lundell F-22
-            "zpi7y8ohw7b5t8ieutka3wi4yw" # Eskil Nyberg F-21
+            "xzt5e9rmtb8b5c78auq3h7bffr", # Elias Lundell @ellundel
+            "zpi7y8ohw7b5t8ieutka3wi4yw", # Eskil Nyberg @eskilny
+            "krir8uax77g58kqzbq5jsxgnpy" # Björn Thiberg @bthiberg
+            ],
+        "styret": [
+            "xzt5e9rmtb8b5c78auq3h7bffr", # Elias Lundell
+            "x1d5x1moetyqjp6q1af3ibkbac", # Niklas Käll
+            "pcnu5yo5bf81xnsw1uwpysunfc", # Rebecca Martinsson
+            "kqfuzsu1wtr43rnwc9wesi4pjr", # Rilind Hoti
+            "jwpei45k4ffbdc8mhr8iyf3syh", # Kajsa Engelfeldt
+            "fchz14cm5ibkzncnsc5amft5ge", # Filip Ek
+            "1a7a3s9um78p98zsz1k35a5k8r" # Erika Sandhagen
+            ],
+        "fn": [
+            "64pa4as6mfro5ea7gmrjfnx4iy", # Jonathan Kenichi Ahrlind
+            "wwpnsya8wfrgt8twu61y5pxbnw" # Maximilian Nordler
+            ],
+        "jamn": [
+            "3yx6i7tk4bgrtf7k4n5gw541jo" # Isac Wänlund
+            ],
+        "fsn": [
+            "f1kbfs9qdffqxbztqtz353gmpw", # Lovisa Tallenmo Keller
+            "87ykcyqzo38cze69rxynqe6kke", # Ludwig Wahlund
+            "z9g8y9yxgpry7b9uozk18mqfua" # Lucas Benson
+            ],
+        "fan": [
+            "unq197dmcjrqfgxk54cc4h9afa", # Hugo Wretling
+            "w4grts7mhjnotpc9zmpkicp6hw" # Elliot Collin
+            ],
+        "fysikalen": [
+            "1p1zbmo1ebbfddypuc8qy1mjje", # Albin Wickman Lundberg
+            "s5kupif6eif9dxyweygbx7tu4h", # Arman Aspromonti
+            "4fs75fz3itbkijezahwto33f3w" # Linnea Stålberg
+            ],
+        "fkm": [
+            "7r84odxya38qxyqn1m9opzop6a", # Emil Wiktorsson
+            "5d85r8gsn7dcjyeo48o4j41cjo", # Katja Borelius
+            "wnq7fw5kdiyiiq5kamz4u7r4oc" # Sam Houghton
+            ],
+        "fcom": [
+            "e64p9dgbxjgeuc83c6eugwpfao" # Mårten Gunnarson Fürstenbach
+            ],
+        "fdev": [
+            "zhrji5nhn3dxigoey9g886xmqh" # William Kraft
+            ],
+        "fart": [
+            "o8mkpfrintf1tng4f6m8wf6x5y" # Wilhelm Carmevik
+            ],
+        "force": [
+            "any44nxbypfjxgkz4ranh9qkpo" # Zofia Lukasiewicz
+            ],
+        "frum": [
+            "not9nx743bda5jpk7sqfiymz3h", # Emmie Zemack
+            "thu6qzqg13bwzpc1acs8sbb66w" # Magdalena Christiansson
+            ],
+        "mottagningen": [
+            "bmzchi357irdif9pcgq9ozpcbc", # Fredrika Lundqvist Åbrink
+            "3y41ocz89fnt5pr8qo4e6kj3xy", # Nessim Khemiri
+            "76n3u5yp9tb53mnsjcky7rs9ih", # Signe Svensson
+            "jxxzg9ybcpyfukcstmdyyo96ue", # Sverker Ernlund
+            "3uir9uh7wfdujxjne7hn3q4qpw", # Lukas Granqvist
+            "6tkf3ofjpbywtksumh6xwn61pr", # Magdalena Björk
+            "yfoh44x7ap893d95q7h4i5t9dr" # Ryll Åman
+            ],
+        "fint": [
+            "fppkbs46rjry5dyf6y5tq84neh" # Arthur Maignan
+            ],
+        "valberedningen": [
             ]
         }
 
 AUTHENTICATED_USERS = {
-        "fan": [],
-        "fart": [],
-        "fcom": [],
-        "fdev": [],
-        "fint": [],
-        "fkm": [],
-        "fn": [],
-        "force": [],
-        "frum": [],
-        "fsn": [],
-        "fysikalen": [],
-        "jamn": [],
-        "mottagningen": [],
-        "styret": [*USERS["mattermästare"]],
-        "valberedningen": []
+        "fan": {*USERS["fan"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fart": {*USERS["fart"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fcom": {*USERS["fcom"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fdev": {*USERS["fdev"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fint": {*USERS["fint"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fkm": {*USERS["fkm"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fn": {*USERS["fn"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "force": {*USERS["force"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "frum": {*USERS["frum"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fsn": {*USERS["fsn"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "fysikalen": {*USERS["fysikalen"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "jamn": {*USERS["jamn"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "mottagningen": {*USERS["mottagningen"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "styret": {*USERS["styret"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]},
+        "valberedningen": {*USERS["valberedningen"], *USERS["styret"], *USERS["mattermästare"], *USERS["fdev"], *USERS["fcom"]}
         }
