82338d3602440e2e82ddeb6f1c194a338c6ae49f
Elias Lundell <36220731+LogFlames@users.noreply.github.com>
2024-01-25 22:31:28 +0100

    Use relative path to __file__

--- gadminintegration/gint.py
+++ gadminintegration/gint.py
@@ -14,7 +14,7 @@ def get_group_members(group, recursive = False, include_subgroups = False):
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "token.json")):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -22,11 +22,11 @@ def get_group_members(group, recursive = False, include_subgroups = False):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
+                    os.path.join(os.path.dirname(__file__), "credentials.json"), SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(os.path.join(os.path.dirname(__file__), "token.json"), "w") as token:
             token.write(creds.to_json())
 
     try:
