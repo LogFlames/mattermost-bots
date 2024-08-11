e0180dff01eb97b76d924bd4a2b7f6822e3b136f
Elias Lundell <elilun03@gmail.com>
2024-08-11 16:12:13 +0200

    add schema to common channels'

--- mottagningen/configuration.py
+++ mottagningen/configuration.py
@@ -1,4 +1,5 @@
 TEAM_ID = "b7hgwxzpzibdtdicycojcsn54w"
 CHANNELS = {
-        "mottagningen_informerar": "hahxyji9nibwub5b9pxex4gy5y"
+        "mottagningen_informerar": "hahxyji9nibwub5b9pxex4gy5y",
+        "mottagningens_schema": "chp6hx43o7nyix4z38aa8657fa"
         }
