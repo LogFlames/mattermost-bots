69a76a2dbbac4f195b900ba6ec515885b35d8571
Elias Lundell <elilun03@gmail.com>
2025-08-25 18:21:39 +0200

    Additional messages

--- scripts/fohsar_messages_to_detele_and_undelete.py
+++ scripts/fohsar_messages_to_detele_and_undelete.py
@@ -32,17 +32,17 @@ blacklisted_messages_2023_2024 = {
         }
 
 blacklisted_messages_2025 = {
-        "martengf": ["kc6ge5uhj78d5n8ktum6o6zkpa", "17354enyrtfc9gwzwdag86sn8o", "mazw6iffdb8mjf67gzigw1jaia", "k6px4en5qb8mmnm37gn9hi873c", "eehnbq1tmprdxkiba8yhqbpymh", "mz5jdwstejfgmgfhctpfbcpw4h"],
+        "martengf": ["kc6ge5uhj78d5n8ktum6o6zkpa", "17354enyrtfc9gwzwdag86sn8o", "mazw6iffdb8mjf67gzigw1jaia", "k6px4en5qb8mmnm37gn9hi873c", "eehnbq1tmprdxkiba8yhqbpymh", "mz5jdwstejfgmgfhctpfbcpw4h", "7dcz5jhegbgajr3ntd4yr57y5o", "kd6puhe3jffjtqksnzcfmdk6aw"],
         "hlennman": ["89npfaxb6jfpumrj7sandg1e7r"],
         "mollyw": ["ofr36mkh5tr7pk88d6965h7k1o", "1r15neoeh3dt3rh46ow58xm4pa"],
-        "signesv": ["rkhx9wghtid6iccgukux9rtt3y", "7gchxbrq6jnsbdm7qpyw4ykf7a", "jz7f9m11fbr8jrfcgrztk8qkkh", "njkh7hbfpbycidy6cei477jatc", "qi76o8kcitdr3ywzs1aq4oookh"],
+        "signesv": ["rkhx9wghtid6iccgukux9rtt3y", "7gchxbrq6jnsbdm7qpyw4ykf7a", "jz7f9m11fbr8jrfcgrztk8qkkh", "njkh7hbfpbycidy6cei477jatc", "qi76o8kcitdr3ywzs1aq4oookh", "erjp4j1ttin63xkt9pcjeb9joy"],
         "sernlund": ["be8mjm9jji8pmjbt3ss69pkqah"],
         "ntiemer": ["jtejg9fedj8xuerkyb11t31w5r", "aqjgbafyiire7e8yupuhho59pr", "us9j9aks3tde8gdmt9tqmig9ma"],
         "m25": ["adb5qhawy7rz9eetuo8gm1sepa", "bjy4ush8nbdtzydx6cu3pyhp6r", "jaanyrzxo7dn9ksy1cizf5psua", "un85pdmbjincdbox8njph76wta", "n4etuspw9injfm8xgy5yith5ky", "1oxyebttebgypmg1ord4fzie8w", "cxzjusbfjidoifeeiscy3em9fo", "8n6jhh6zgfnhzn7ycjxnkd5h4a", "r49jtxxq1pnxfn7qxocjmubitc", "9syd9fw1ztdupg84b5k5tcyjiy", "gwfbttf1g3bnzmwmfwqu4bz9oh", "awb8ix4p93y68fsr5wu5r83a4a", "tr8rqukynfyzjx4od3mc9m9ary", "repou6549jgifdh69dwmwz78tw", "9h7hxfjxq78a9xoarmnz4yxufe", "53yeemnkgbnf8gr9riiirxnguh", "paexm3yxx38yirg8s9isus8o5y", "g66p8zkxw78cdfhuramo3oykxo", "ynh8s1smzir1texpfkx55eqtqw", "86ucz4nbrfrgjybktqzhd3c9hh", "r4yn8gzenjf8dnwssnwbazjg5y", "m5hphb7z5byddy5wb77zfwsfyy", "y4pchh3mm7ybtyhc976jt5usia", "owbufgibnind7yuxn7az6qd4kr", "tj3kcak9pp83zk5k9atjf7kxmw", "ujuzmo7b8fd85nan4wmn3gbmey"],
         "sjsak": ["nfhra1s5sibgxk836r7867n1rc"],
         "ltrolin": ["ycphyuxsj3d6d8xo5uz7xuwyhe"],
         "ebbalo": ["ybqwcspu7t89bfffy99fg3mqzy"],
-        "juliasve": ["irbhdrmn8ffyur18rjco96y51o", "9uyyceuuyinojpyggzfxmhopse", "9kyj5d6zb3b48nwwh3bwss4nde", "11cd83nentyp3kor9hh74ekmir"],
+        "juliasve": ["irbhdrmn8ffyur18rjco96y51o", "9uyyceuuyinojpyggzfxmhopse", "9kyj5d6zb3b48nwwh3bwss4nde", "11cd83nentyp3kor9hh74ekmir", "ko53of6w1pypjka8bryeziztky"],
         "haggbr": ["c33ipzoebtrcmesu6unf1f6qnc"],
         "alvaah": ["pefh44t3gfncbnksm18wjkxnco", "itbe97pjs7fixku1d68hdqou4a"],
         "lukasgra": ["mookuy4unjncdf3uizq89jp43h", "8xaeukt1kjgitpchykoe4u5z6c"],
@@ -50,12 +50,20 @@ blacklisted_messages_2025 = {
         "aleung": ["o18eh6owdfgf5bsstxqg8q7bqe"],
         "emmiez": ["o8tq664egb83zr4b839th7sb1w", "5sydck8a4jrwdnw8ux3tzhcexo"],
         "chen9": ["sh9oktec8tb55yi4qkfh4c5cna", "7bdxahj38iyw5f9jzuc7dnpmza"],
-        "albinwl": ["nowkmong8jny7eqzgqbi84yqzy"],
+        "albinwl": ["nowkmong8jny7eqzgqbi84yqzy", "8pym7p6pztfdjr7ss6681kmfpw", "jj1qwkim4bn7dkppc654f5anzy"],
         "armanas": ["7rm8dz5qbp8k3fge9msh3ta3xo"],
         "magdb": ["xpmqb8ox53byznu9ked9higefh"],
         "elbergst": ["meisaho9yin3zfo3nt3mfp6kxo", "i8tt7qo8g7n88bgoqtfxtgtnrh", "zntz86ne3fdzik498spudzcete"],
         "lwhalund": ["3smrsnrqoif6pmakg86gq5q1dy"],
-        "hoti": ["nnot4xjexbft3gwobk6mi66u8r"]
+        "hoti": ["nnot4xjexbft3gwobk6mi66u8r", "sut1i3arwirm9j9fryzb6j5s1h"],
+        "jjl": ["p66ip4yo8b8afn6fyaj716jama"],
+        "wbergst": ["635frd9kbp8wfk7k4gbgpqe86r"],
+        "davpette": ["n533bbemb3np9p194k4er8xk8e"],
+        "katjabo": ["qcjkc9fkwire9rqsxazuxhaqew", "dpqdpu3p5ty8jfjrxyw7y6h4xe"],
+        "hugowr": ["r5e6d3m4o7na8xh5xqsyhmdpuc"],
+        "samho": ["53wf9pdpbjyq3mkareu8ak4gxh", "o9an7rixmj8m9mdg4h97driwce"],
+        "elliotc": ["i71asgrw13gtjjh644xr4rchmo"],
+        "vmel": ["131r549ap3r5xybtxjiyzqpkwc"]
         }
 
 blacklisted_messages = {}
