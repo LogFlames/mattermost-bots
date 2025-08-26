8d128ee8c8dd77d329324bbfff6ded12c6b35a34
Elias Lundell <elilun03@gmail.com>
2025-08-26 21:17:12 +0200

    Add missing :

--- website_publisher/main.py
+++ website_publisher/main.py
@@ -32,7 +32,7 @@ def remove_emojis(text):
 def transform_mattermost_message_to_website_html(driver, message, namnd, lang):
     formatted_name = NAMND_FORMATTED_NAME_EN[namnd] if lang == "en" else NAMND_FORMATTED_NAME[namnd]
     responsible = "Responsible Committee" if lang == "en" else "Ansvarig nämnd: "
-    ansvarig_namnd = f"<h6>{responsible} <a href=\"mailto:{NAMND_EMAIL_MAP[namnd]}\">{formatted_name}</a></h6>"
+    ansvarig_namnd = f"<h6>{responsible}: <a href=\"mailto:{NAMND_EMAIL_MAP[namnd]}\">{formatted_name}</a></h6>"
 
     return convert_markdown(
             html_escape_codes(
